98909aba1c70c23f83443626dc936dbb089d33e0
hrandhi-5303 <rhemasre.sree@gmail.com>
2025-05-29 16:02:12 -0500

    Done Two-Pointers-1

---
+++ Problem3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self,height):
+        left,right=0,len(height)-1
+        max_area=0
+
+        while left<right:
+            width=right-left
+            min_height=min(height[left],height[right])
+            area=width*min_height
+            max_area=max(max_area,area)
+
+            if height[left]<height[right]:
+                left+=1
+            else:
+                right-=1
+
+        return max_area
+    
+sol=Solution()
+print(sol.maxArea(height = [1,8,6,2,5,4,8,3,7]))
+print(sol.maxArea(height = [1,1]))
